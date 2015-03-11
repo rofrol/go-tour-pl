23b1ab23b2f15e551c91d1d23d72fadc1f688534
Roman Frołow <rofrol@gmail.com>
2015-03-11 16:32:05 +0100

    translation of js

--- static/js/values.js
+++ static/js/values.js
@@ -9,43 +9,43 @@ angular.module('tour.values', []).
 // List of modules with description and lessons in it.
 value('tableOfContents', [{
     'id': 'mechanics',
-    'title': 'Using the tour',
-    'description': '<p>Welcome to a tour of the <a href="http://golang.org">Go programming language</a>. The tour covers the most important features of the language, mainly:</p>',
+    'title': 'Używanie wycieczki',
+    'description': '<p>Witaj na wycieczce po <a href="http://golang.org">języku programowania Go</a>. Wycieczka pokrywa większość ważnych cech języka, głównie:</p>',
     'lessons': ['welcome']
 }, {
     'id': 'basics',
-    'title': 'Basics',
-    'description': '<p>The starting point, learn all the basics of the language.</p><p>Declaring variables, calling functions, and all the things you need to know before moving to the next lessons.</p>',
+    'title': 'Podstawy',
+    'description': '<p>Punkt startowy, naucz się podstaw języka.</p><p>Deklarowanie zmiennych, wywoływanie funkcji oraz wszystkie rzeczy, które potrzebujesz znać, zanim przejdziesz do następnych lekcji.</p>',
     'lessons': ['basics', 'flowcontrol', 'moretypes']
 }, {
     'id': 'methods',
-    'title': 'Methods and interfaces',
-    'description': '<p>Learn how to define methods on types, how to declare interfaces, and how to put everything together.</p>',
+    'title': 'Metody i interfejsy',
+    'description': '<p>Naucz się definiować metody na typach, jak deklarować interfejsy oraz jak złożyć to wszystko razem.</p>',
     'lessons': ['methods']
 }, {
     'id': 'concurrency',
-    'title': 'Concurrency',
-    'description': '<p>Go provides concurrency features as part of the core language.</p><p>This module goes over goroutines and channels, and how they are used to implement different concurrency patterns.</p>',
+    'title': 'Współbieżność',
+    'description': '<p>Go udostępnia cechy współbieżności jako część rdzenia języka.</p><p>Ten moduł przechodzi przez gorutyny i kanały oraz jak one są użyte do implementacji różnych wzorców współbieżności.</p>',
     'lessons': ['concurrency']
 }]).
 
 // translation
 value('translation', {
-    'off': 'off',
-    'on': 'on',
-    'syntax': 'Syntax-Highlighting',
-    'lineno': 'Line-Numbers',
-    'reset': 'Reset Slide',
-    'format': 'Format Source Code',
-    'kill': 'Kill Program',
-    'run': 'Run',
-    'compile': 'Compile and Run',
-    'more': 'Options',
-    'toc': 'Table of Contents',
-    'prev': 'Previous',
-    'next': 'Next',
-    'waiting': 'Waiting for remote server...',
-    'errcomm': 'Error communicating with remote server.',
+    'off': 'wyłącz',
+    'on': 'włącz',
+    'syntax': 'Podświetlanie składni',
+    'lineno': 'Numery linii',
+    'reset': 'Zresetuj slajd',
+    'format': 'Sformatuj kod źródłowy',
+    'kill': 'Zabij program',
+    'run': 'Uruchom',
+    'compile': 'Skompiluj i uruchom',
+    'more': 'Opcje',
+    'toc': 'Spis treści',
+    'prev': 'Poprzedni',
+    'next': 'Następny',
+    'waiting': 'Czekanie na zdalny serwer...',
+    'errcomm': 'Błąd komuniacji ze zdalnym serwerem.',
 }).
 
 // Config for codemirror plugin
